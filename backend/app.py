e86ac907ec0c07986423e77ed440f16051c0cdf8
gowthambvs83-droid <gowthambvs83@gmail.com>
2025-12-17 16:00:40 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,18 @@
-# Import Flask class (used to create server)
 from flask import Flask, request, jsonify
-
-# Import CORS to allow frontend to talk to backend
 from flask_cors import CORS
+import os
 
-# Create Flask app object
 app = Flask(__name__)
-
-# Enable CORS for all routes
 CORS(app)
 
-# Create API endpoint: /calculate
 @app.route("/calculate", methods=["POST"])
 def calculate():
-    # Read JSON data sent from JavaScript
     data = request.json
 
-    # Extract values from JSON
     a = float(data["num1"])
     b = float(data["num2"])
     op = data["operation"]
 
-    # Perform calculation
     if op == "+":
         result = a + b
     elif op == "-":
@@ -33,11 +24,8 @@ def calculate():
     else:
         result = "Invalid operation"
 
-    # Send result back as JSON
     return jsonify({"result": result})
 
-
-# Run server
 if __name__ == "__main__":
-    app.run()
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
