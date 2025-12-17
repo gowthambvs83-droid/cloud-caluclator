2e756e1e48e67802e51aa49bb583f4969a3ebfd7
gowthambvs83-droid <gowthambvs83@gmail.com>
2025-12-17 15:47:03 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,25 @@
+function calculate() {
+    // Read values from HTML
+    const n1 = document.getElementById("num1").value;
+    const n2 = document.getElementById("num2").value;
+    const op = document.getElementById("operation").value;
+
+    // Send data to Python backend
+    fetch("https://cloud-calculator.onrender.com/calculate", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            num1: n1,
+            num2: n2,
+            operation: op
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("result").innerText =
+            "Result: " + data.result;
+    });
+}
 
